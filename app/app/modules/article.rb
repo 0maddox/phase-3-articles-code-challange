fea637c374adb1ca74637845eceb439e322bb7d7
nicholas <nickkiim7@gmail.com>
2023-05-29 15:30:00 +0300

    added articles.rb

---
+++ app/app/modules/article.rb
@@ -0,0 +1,28 @@
+require 'pry'
+#    require_relative  "./magazine"
+#    require_relative   "./author"
+class Article 
+
+    attr_reader :title, :author, :magazine
+
+    @@all = []
+
+    def initialize ( author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title  = title
+        @@all << self
+    end
+
+    def self.all
+        @@all 
+    end
+
+
+end
+
+
+
+
+end
+binding .pry
